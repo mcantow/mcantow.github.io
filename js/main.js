011775a93d4e29666c358425b7e35382e6635df2
mcantow <mcantow@mit.edu>
2024-07-29 19:27:56 -0400

    made side menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for your message!');
+document.addEventListener("DOMContentLoaded", function() {
+    // the video
+    const video = document.getElementById('background-video');
+    const repeatStartTime = 5; // Time in seconds to start the repeat from
+    video.addEventListener('ended', () => {
+        video.currentTime = repeatStartTime;
+        video.play();
+    });
+    let expandedNavContainer = document.getElementById("expandedNavContainer")
+    // open the nav menu
+    document.getElementById("burger").onclick = function(){
+        expandedNavContainer.classList.add("expanded");
+    };
+    // close the nav menu
+    document.getElementById("closeExpandedNavSvg").onclick = function(){
+        expandedNavContainer.classList.remove("expanded");
+    };
 });
